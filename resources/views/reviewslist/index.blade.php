07e6bc1684fe404cc7689f12f82b6d88b5d82ab7
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-05 17:52:05 +0000

    testing git connections

--- resources/views/reviewslist/index.blade.php
+++ resources/views/reviewslist/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          Digital Reviews
+          Digital Reviews test git
        </h2>
     </x-slot>
     <div>
