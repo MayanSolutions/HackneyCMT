e91f20c5e0be8e232975036bb2597fce717c6887
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-05 20:35:07 +0000

    dashboard under development

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,24 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                Under Development
+                <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                             <div class="flex flex-col">
+                                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                   <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                      <div class="shadow overflow-hidden border-b border-gray-200 ">
+                                       <h2 class="mb-2 mt-4 ml-6 font-extrabold float-left tracking-tight text-gray-700">
+                                           Digital Annual Review Surveys
+                                         </h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>
