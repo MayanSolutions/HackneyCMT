eedd845b71a5dd30b5c83f909bbdcab0f3e7796d
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-13 19:54:16 +0000

    email blade added logo

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="{{url('/images/MayanSoftwareSolutionsLogo.png')}}" alt="Logo" style="height: 220px; width: 220px;"></a>
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
