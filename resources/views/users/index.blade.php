80ed1ebb349b526b1c1af9305f4691c4c8dbc815
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-25 11:02:15 +0000

    user index formatting fix

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -22,7 +22,7 @@
                       <div class="flex flex-col">
                          <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                               <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                               <div class="shadow overflow-hidden border-b border-gray-200">
                                 @if (count($users) > 0)
                                 <h2 class="mb-2 mt-4 ml-6 font-extrabold float-left tracking-tight text-gray-700">
                                     Active Accounts
@@ -42,6 +42,7 @@
                                       </button>
                                     </div>
                                  </form>
+                               </div>
                                   <table class="min-w-full divide-y divide-gray-200">
                                      <thead class="bg-gray-50">
                                         <tr>
