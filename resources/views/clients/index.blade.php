6df073d897828e0163a5098a24a4825267e0dec1
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-25 10:58:22 +0000

    formatting fix

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -23,7 +23,7 @@
                         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                                <div class="shadow overflow-hidden border-b border-gray-200 ">
-                                    <h2 class="mb-2 mt-4 ml-6 text-lg font-extrabold float-left tracking-tight text-gray-900">
+                                    <h2 class="mb-2 mt-4 ml-6 font-extrabold float-left tracking-tight text-gray-700">
                                         Tenant Management Organisations
                                     </h2>
                                     <form action="{{ route('clients.index') }}" method="GET" role="search">
