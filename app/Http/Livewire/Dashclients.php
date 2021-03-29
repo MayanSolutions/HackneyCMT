c7a8586a5c3cdf9974953d8cc4410da86ebb70f0
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-29 19:43:41 +0100

    removed with estates

--- app/Http/Livewire/Dashclients.php
+++ app/Http/Livewire/Dashclients.php
@@ -14,6 +14,6 @@ class Dashclients extends Component
     public function render()
     {
         return view('livewire.dashclients',
-    ['clients'=>clients::with('members')->with('EstateDetails')->where('user_id', auth()->user()->id )->paginate(3)]);
+    ['clients'=>clients::with('members')->where('user_id', auth()->user()->id )->paginate(3)]);
     }
 }
