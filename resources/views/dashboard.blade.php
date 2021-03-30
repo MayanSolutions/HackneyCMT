d0678f522f85823539ca6d964d2477846a27934c
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-30 11:23:57 +0100

    added more html formatting

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,13 +6,13 @@
     </x-slot>
 
     <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-        <div class="bg-gray-100 w-100 items-center flex">
+        <div class="bg-gray-100 w-100 items-center mb-4">
             <div class="grid grid-cols-3 grid-rows-1 gap-6">
                 <div class="col-span-1">@livewire('dashprofile')</div>
                 <div class="col-span-2">@livewire('dashnotifications')</div>
             </div>
         </div>
-        <div class="bg-gray-100 w-100 items-center flex">
+        <div class="bg-gray-100 w-100 items-center mb-4">
         <div class="grid grid-cols-3 grid-rows-2 gap-6">
             <div class="col-span-2">@livewire('dashclients')</div>
             <div class="col-span-1"></div>
