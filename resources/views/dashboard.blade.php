556440a0e9948d786915cdaf08672c7c7707b381
Mayan Solutions <63148157+MayanSolutions@users.noreply.github.com>
2021-03-30 11:52:47 +0100

    removed dashclients

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div class="bg-gray-100 w-100 items-center mb-4">
         <div class="grid grid-cols-3 grid-rows-2 gap-6">
-            <div class="col-span-2">@livewire('dashclients')</div>
+            <div class="col-span-2"></div>
             <div class="col-span-1"></div>
         </div>
         </div>
